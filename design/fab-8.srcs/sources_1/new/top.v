6809cca23109fc85e7a43c6a0218f7e2e7fd4557
inixyz <alexandru.ene2702@gmail.com>
2023-07-27 21:03:29 +0300

    Initial commit

---
+++ design/fab-8.srcs/sources_1/new/top.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/27/2023 09:02:04 PM
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(
+
+    );
+endmodule
