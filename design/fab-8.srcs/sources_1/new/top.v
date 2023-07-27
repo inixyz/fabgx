e086090ce83da919da074304f61fabaea7c9822b
inixyz <alexandru.ene2702@gmail.com>
2023-07-27 21:23:04 +0300

    ram

--- design/fab-8.srcs/sources_1/new/top.v
+++ design/fab-8.srcs/sources_1/new/top.v
@@ -1,26 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/27/2023 09:02:04 PM
-// Design Name: 
-// Module Name: top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+module top();
 
-
-module top(
-
-    );
 endmodule
