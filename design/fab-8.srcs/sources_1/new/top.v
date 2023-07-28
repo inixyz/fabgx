3e3b3bb9d4bf2fb01504165458bfbdcc23575555
inixyz <alexandru.ene2702@gmail.com>
2023-07-28 11:02:56 +0300

    start top design

--- design/fab-8.srcs/sources_1/new/top.v
+++ design/fab-8.srcs/sources_1/new/top.v
@@ -1,3 +1,14 @@
 module top();
 
+	reg[7:0] registers[0:15];
+
+	reg[23:0] instruction;
+
+	always @(posedge clk) begin
+		if(instruction[0:7] == ADD) begin
+			registers[instruction[8:11]] = 
+		end
+	end
+
+
 endmodule
