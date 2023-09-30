41d6c1d353c39feb82be73a961e962d6c05b2ed5
inixyz <alexandru.ene2702@gmail.com>
2023-09-30 22:07:42 +0300

    line primitive

--- examples/primitives.c
+++ examples/primitives.c
@@ -8,8 +8,8 @@ int main(){
 
 	fabgx_fill(&surface, (fabgx_color){50, 50, 50, 255});
 
-	fabgx_rect(&surface, (fabgx_vec2){150, 150}, (fabgx_vec2){200, 200},
-		(fabgx_color){0, 255, 0, 255});
+	fabgx_line(&surface, (fabgx_vec2){200, 200}, (fabgx_vec2){50, 150},
+		(fabgx_color){255, 0, 0, 255});
 
 	fabgx_save_to_ppm(&surface, "examples/primitives.ppm");
 }
--- fabgx.h
+++ fabgx.h
@@ -2,6 +2,7 @@
 #define FABGX_H
 
 #include <stdint.h>
+#include <stdlib.h>
 
 typedef struct{
 	uint8_t r, g, b, a;
@@ -54,6 +55,34 @@ void fabgx_rect(fabgx_surface* surface, const fabgx_vec2 pos,
 	for(unsigned int y = pos.y; y < end_pos.y; y++){
 		for(unsigned int x = pos.y; x < end_pos.x; x++){
 			fabgx_pixel(surface, (fabgx_vec2){x, y}, color);
+		}
+	}
+}
+
+void fabgx_line(fabgx_surface* surface, fabgx_vec2 start_pos,
+	const fabgx_vec2 end_pos, const fabgx_color color){
+
+	// https://en.wikipedia.org/wiki/Bresenham's_line_algorithm
+	const int dx = abs(end_pos.x - start_pos.x);
+	const int dy = -abs(end_pos.y - start_pos.y);
+	const int sx = start_pos.x < end_pos.x ? 1 : -1;
+	const int sy = start_pos.y < end_pos.y ? 1 : -1;
+
+	int e2, error = dx + dy;
+
+	while(1){
+		fabgx_pixel(surface, start_pos, color);
+		if(start_pos.x == end_pos.x && start_pos.y == end_pos.y) break;
+		e2 = error * 2;
+		if(e2 >= dy){
+			if(start_pos.x == end_pos.x) break;
+			error += dy;
+			start_pos.x += sx;
+		}
+		if(e2 <= dx){
+			if(start_pos.y == end_pos.y) break;
+			error += dx;
+			start_pos.y += sy;
 		}
 	}
 }
